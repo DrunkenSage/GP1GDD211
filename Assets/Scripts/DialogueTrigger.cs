3bcfeb6a028d9a787ce4dfdab2aa5dba5c60a0e8
DrunkenSage <60531591+DrunkenSage@users.noreply.github.com>
2021-03-24 15:03:31 -0400

    VillageAndDialogueScripts

---
+++ Assets/Scripts/DialogueTrigger.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dialogueTrigger : MonoBehaviour
+{
+    public _Dialogue dialogue;
+
+    public void TriggerDialogue()
+    {
+        FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
+    }
+}
