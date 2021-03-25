5e732f29c16af95ecaa0bc222c3e9ea78bf05783
DrunkenSage <60531591+DrunkenSage@users.noreply.github.com>
2021-03-25 12:41:54 -0400

    Publication One

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -12,8 +12,8 @@ public class Player : MonoBehaviour
     {
         inventory = new Inventory();
         uiInventory.SetInventory(inventory);
-        ItemWorld.SpawnItemWorld(new Vector3(1, 1), new Item { itemType = Item.ItemType.Sword, amount = 1 });
-        ItemWorld.SpawnItemWorld(new Vector3(-1, 1), new Item { itemType = Item.ItemType.Journal, amount = 1 });
+        ItemWorld.SpawnItemWorld(new Vector3(2, 1), new Item { itemType = Item.ItemType.Sword, amount = 1 });
+        ItemWorld.SpawnItemWorld(new Vector3(-2, 1), new Item { itemType = Item.ItemType.Journal, amount = 1 });
     }
     private void OnTriggerEnter2D(Collider2D collider)
     {
